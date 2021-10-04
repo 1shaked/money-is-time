0fc399dfb18f808ef648aa0f71f45a9525c16e58
1shaked <shaked1hen@gmail.com>
2021-10-04 10:12:05 +0300

    added the if open colors and text

--- lib/components/timerPage/action_button_timer.dart
+++ lib/components/timerPage/action_button_timer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:moneytime/services/services.dart';
+import 'package:provider/provider.dart';
 
 class ActionButtonTimer extends StatelessWidget {
   const ActionButtonTimer({
@@ -7,19 +9,26 @@ class ActionButtonTimer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 202,
-      height: 68,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(60),
-        color: Color.fromRGBO(121, 210, 141, 1),
-      ),
-      child: Center(
-        child: Text(
-          'start',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 32,
+    return GestureDetector(
+      onTap: () {
+        Provider.of<JobsManager>(context, listen: false).actionButtonClick();
+      },
+      child: Container(
+        width: 202,
+        height: 68,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(60),
+          color: Provider.of<JobsManager>(context).isActive
+              ? Colors.redAccent
+              : const Color.fromRGBO(121, 210, 141, 1),
+        ),
+        child: Center(
+          child: Text(
+            Provider.of<JobsManager>(context).isActive ? 'Stop' : 'Start',
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 32,
+            ),
           ),
         ),
       ),
